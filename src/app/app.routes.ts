4deb6f261f437352d369a73f2bf3f06a31300275
Ajay25092002 <ajay.kumar@maargaai.com>
2026-08-11 18:25:27 +0530

    fix: set supported node version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main/main-layout.component';
+import { HomeComponent } from './pages/home/home';
 
 export const routes: Routes = [
     {
-        path: "",
-        redirectTo: "Home",
-        pathMatch: "full"
-    },
-    {
-        path: "Home",
+        path: '',
         component: MainLayoutComponent,
-    }
+
+        children: [
+            {
+                path: '',
+                redirectTo: 'Home',
+                pathMatch: 'full',
+            },
+            {
+                path: 'Home',
+                title: 'MMC Innovations | Home',
+                component: HomeComponent,
+            },
+        ],
+    },
 ];
